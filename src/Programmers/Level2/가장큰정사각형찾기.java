ab45db7a57c9a87e2a15bab8d3360a57ef3efd88
TaeGyun <taekuen98@naver.com>
2021-06-24 22:25:39 +0900

    21.06.24

---
+++ src/Programmers/Level2/가장큰정사각형찾기.java
@@ -0,0 +1,35 @@
+package Programmers.Level2;
+
+public class 가장큰정사각형찾기 {
+
+    public int solution(int[][] board){
+        int answer =0;
+        int row = board.length;
+        int col = board[0].length;
+        int[][] map = new int[row+1][col+1];
+
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                map[i+1][j+1] = board[i][j];
+            }
+        }
+
+        for(int i=1; i<=row; i++){
+            for(int j=1; j<=col; j++){
+                if(map[i][j] != 0){
+                    map[i][j] = Math.min(Math.min(map[i-1][j], map[i][j-1]),
+                            map[i-1][j-1])+1;
+                    answer = Math.max(answer, map[i][j]);
+                }
+            }
+        }
+
+        return answer * answer;
+    }
+
+
+    public static void main(String[] args){
+        가장큰정사각형찾기 T = new 가장큰정사각형찾기();
+        int[][] board = {{0,1,1,1}, {1,1,1,1},{1,1,1,1},{0,0,1,0}};
+    }
+}
