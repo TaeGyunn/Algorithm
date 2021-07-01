bd99a3770439b2c4b4c33e35a4f52b65c0283e2b
TaeGyun <taekuen98@naver.com>
2021-07-01 21:25:28 +0900

    21.07.01

---
+++ src/Programmers/Level2/프로그래밍1.java
@@ -0,0 +1,32 @@
+package Programmers.Level2;
+
+public class 프로그래밍1 {
+
+    public int[] solution(int[][] v) {
+        int x;
+        int y;
+
+        if(v[0][0]==v[2][0]){
+            x=v[1][0];
+        }else if(v[1][0]==v[2][0]){
+            x=v[0][0];
+        }else{
+            x=v[2][0];
+        }
+
+        if(v[0][1]==v[1][1]){
+            y=v[2][1];
+        }else if(v[1][1]==v[2][1]){
+            y=v[0][1];
+        }else{
+            y=v[1][1];
+        }
+
+        int[] answer = {x,y};
+        return answer;
+    }
+
+    public static void main(String[] args){
+
+    }
+}
